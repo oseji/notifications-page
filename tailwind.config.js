a8e3e3886dcdc1da32837cf2641fd65235351e25
oseji <jiade1233@gmail.com>
2023-04-16 10:28:45 +0100

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
